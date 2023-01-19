86f88c9220e5a25eef529c478e43f8ae5e58b76d
unknown <wrightkuku10@gmail.com>
2023-01-19 19:30:10 +0300

    initial commit

---
+++ recursion.js
@@ -0,0 +1,10 @@
+// javascript program to calculate the factorial of any number
+function factorial(n) {
+    if (n == 1 || n==0){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1)
+    }
+}
+console.log(factorial(6));
